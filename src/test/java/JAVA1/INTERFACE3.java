d71bc321cf515e5469421095c03fff37c2d35bb5
test <test@gmail.com>
2023-09-20 21:22:11 -0400

    git practice

---
+++ src/test/java/JAVA1/INTERFACE3.java
@@ -0,0 +1,34 @@
+package JAVA1;
+
+public class INTERFACE3 {
+
+	import com.nexttech.JAVA.INTERFACEPRACTICE;
+
+	public class INTERFACE3 implements INTERFACEPRACTICE {
+
+		public static void main(String[] args) {
+			
+
+		}
+
+		public void method1() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void method2() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void method3() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void method4() {
+			// TODO Auto-generated method stub
+			
+		}
+
+	}
